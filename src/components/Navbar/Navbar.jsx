ea4d8a51931290c846e707132ee0b8ab4d848d9f
o-Erebus <neelkhatri45@gmail.com>
2025-01-11 22:59:06 +0530

    Added Visionary Logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import { navLinks } from '../../constants/navigation';
 import { Moon, Sun } from 'lucide-react';
 import { useTheme } from "../../hooks/DarkMode/DarkMode.jsx";
 
+
 const Navbar = ({ mobileMenuOpen, setMobileMenuOpen }) => {
     const { isDark, toggleTheme } = useTheme();
     const [isVisible, setIsVisible] = useState(true); // Track navbar visibility
@@ -55,7 +56,13 @@ const Navbar = ({ mobileMenuOpen, setMobileMenuOpen }) => {
             <div className="max-w-7xl mx-auto px-4">
                 <div className="flex justify-between items-center h-16">
                     <Link to="/" className="flex items-center gap-4">
-                        <div className="bg-cyan-500 text-black px-4 py-2 font-mono font-bold">VC</div>
+                        <div className="px-4 py-2 rounded-lg">
+                            <img
+                                src="../../../public/logo.png"
+                                alt="VC Logo"
+                                className="h-10 object-contain"
+                            />
+                        </div>
                         <span
                             className={`font-mono hidden md:block ${
                                 isDark ? 'text-cyan-500' : 'text-black'
