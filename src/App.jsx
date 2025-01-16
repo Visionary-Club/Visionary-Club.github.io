d6ccbae32b3b8b9160e4f3283f5816d1834f120c
o-Erebus <neelkhatri45@gmail.com>
2025-01-17 03:13:59 +0530

    Added New Routes

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,20 @@ import React, {useCallback, useState} from "react";
 import {loadSlim} from "tsparticles-slim";
 import Navbar from "./components/Navbar/Navbar.jsx";
 import {ThemeProvider} from "./providers/ThemeProvider/ThemeProvider.jsx";
+import EventsPage from "./New/components/Events.jsx";
+
+import HackathonPage from "./New/components/Hackathon.jsx";
+import MembersPage from "./pages/Members/MembersPage.jsx";
+import {useTheme} from "./hooks/DarkMode/DarkMode.jsx";
+import JoinUsPage from "./pages/JoinUs/JoinUsPage.jsx";
+import ContactUsPage from "./pages/ContactUs/ContactUsPage.jsx";
 
 const App = () => {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
+
     return (
+
         <ThemeProvider>
             <Router>
                 <Navbar mobileMenuOpen={mobileMenuOpen} setMobileMenuOpen={setMobileMenuOpen} />
@@ -20,6 +29,11 @@ const App = () => {
                     <Route path="/" element={<HomePage />} />
                     <Route path="/projects" element={<ProjectsPage />} />
                     <Route path="*" element={<NotFoundPage />} />
+                    {/*<Route path="/events" element={<EventsPage />} />*/}
+                    <Route path="/members" element={<MembersPage />} />
+                    <Route path="/hackathon" element={<HackathonPage />} />
+                    <Route path={'/join' } element={<JoinUsPage />} />
+                    <Route path={'/contact' } element={<ContactUsPage />} />
                 </Routes>
             </Router>
         </ThemeProvider>
