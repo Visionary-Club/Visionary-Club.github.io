d70d30e82f6a58712e5ef14ad139520eb7465d43
Neel Jayesh Khatri <134832151+o-Erebus@users.noreply.github.com>
2025-01-23 23:36:40 +0530

    Update ProjectsPage.jsx

--- src/pages/Project/ProjectsPage.jsx
+++ src/pages/Project/ProjectsPage.jsx
@@ -13,7 +13,7 @@ const ProjectsPage = () => {
     const [selectedProject, setSelectedProject] = useState(null);
 
     useEffect(() => {
-        axios.get('https://raw.githubusercontent.com/o-Erebus/Visionary-Club.github.io/refs/heads/main/public/projects.json')
+        axios.get('https://raw.githubusercontent.com/Visionary-Club/Visionary-Club.github.io/refs/heads/main/public/projects.json')
         //axios.get('/projects.json')
             .then(response => {
                 const sortedProjects = response.data.sort(
