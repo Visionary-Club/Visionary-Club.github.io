62366bdd74be24fc37172349917ff5093ef01d2d
Neel Jayesh Khatri <134832151+o-Erebus@users.noreply.github.com>
2025-01-23 23:36:08 +0530

    Update generateProjectsJson.cjs

--- generateProjectsJson.cjs
+++ generateProjectsJson.cjs
@@ -3,7 +3,7 @@ const path = require('path');
 
 const projectsDir = path.join(__dirname, 'public', 'Projects');
 const projectsJsonPath = path.join(__dirname, 'public', 'projects.json');
-const rawBaseUrl = 'https://raw.githubusercontent.com/o-Erebus/Visionary-Club.github.io/main/public/Projects';
+const rawBaseUrl = 'https://raw.githubusercontent.com/Visionary-Club/Visionary-Club.github.io/main/public/Projects';
 //const rawBaseUrl = "Projects";
 const generateProjectsJson = async () => {
     const projectDirs = await fs.readdir(projectsDir);
